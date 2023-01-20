50a1fda26d17150a0eeaad844f6ddfe4ee520c08
unknown <hernan53865@gmail.com>
2023-01-19 22:19:12 -0500

    gh-pages added

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hernan53865/rick-and-morty.git main:gh-pages
+git push -f git@github.com:hernan53865/react-rick-and-morty.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://hernan53865.github.io/rick-and-morty/",
+  base:"https://hernan53865.github.io/react-rick-and-morty/",
   plugins: [react()],
 })
